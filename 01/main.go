d4d12b4482fb7b48d66b706f958c9c780f67dcdd
tchwojkofrank <tom@chwojkofrank.com>
2023-11-30 21:25:28 -0800

    Day 1 parts one and two

--- 01/main.go
+++ 01/main.go
@@ -8,6 +8,21 @@ import (
 	"time"
 )
 
+const (
+	one   = "one"
+	two   = "two"
+	three = "three"
+	four  = "four"
+	five  = "five"
+	six   = "six"
+	seven = "seven"
+	eight = "eight"
+	nine  = "nine"
+	zero  = "zero"
+)
+
+var numberMap = map[string]int{one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9, zero: 0}
+
 func readInput(fname string) string {
 	content, err := os.ReadFile(fname)
 	if err != nil {
@@ -33,5 +48,52 @@ func main() {
 	fmt.Printf("Running time: %v\n", end.Sub(start))
 }
 
+func getFirstDigitFromLine(line string) int {
+	for i, c := range line {
+		if c >= '0' && c <= '9' {
+			return int(c - '0')
+		}
+		for number, value := range numberMap {
+			if strings.HasPrefix(line[i:], number) {
+				return value
+			}
+		}
+	}
+	return -1
+}
+
+func getLastDigitFromLine(line string) int {
+	for i := len(line) - 1; i >= 0; i-- {
+		c := line[i]
+		if c >= '0' && c <= '9' {
+			return int(c - '0')
+		}
+		for number, value := range numberMap {
+			if strings.HasPrefix(line[i:], number) {
+				return value
+			}
+		}
+	}
+	return -1
+}
+
+func getLineValue(line string) int {
+	first := getFirstDigitFromLine(line)
+	last := getLastDigitFromLine(line)
+	fmt.Printf("First: %d, Last: %d\n", first, last)
+	return first*10 + last
+}
+
+func getSumFromLines(lines []string) int {
+	sum := 0
+	for _, line := range lines {
+		sum += getLineValue(line)
+	}
+	return sum
+}
+
 func run(input string) {
+	lines := strings.Split(input, "\n")
+	linesValue := getSumFromLines(lines)
+	fmt.Printf("Sum of all lines: %d\n", linesValue)
 }

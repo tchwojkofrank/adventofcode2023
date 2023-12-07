b5c86df069d40a9cdb5c43c29618ee5433683d3a
tchwojkofrank <tom@chwojkofrank.com>
2023-12-06 22:39:34 -0800

    adjustments

--- interval/interval.go
+++ interval/interval.go
@@ -3,41 +3,45 @@ package interval
 import "errors"
 
 type Interval struct {
-	start int
-	end   int
+	Start int
+	End   int
 }
 
-func (a Interval) intersect(b Interval) (Interval, error) {
+func (a Interval) Intersection(b Interval) (Interval, error) {
 	var result Interval
 	var err error
-	if a.end < b.start || b.end < a.start {
+	if a.End < b.Start || b.End < a.Start {
 		result = a
 		err = errors.New("No intersection")
 		return result, err
 	}
-	return Interval{max(a.start, b.start), min(a.end, b.end)}, nil
+	return Interval{max(a.Start, b.Start), min(a.End, b.End)}, nil
 }
 
-func (a Interval) union(b Interval) (Interval, error) {
-	if a.end < b.start || b.end < a.start {
+func (a Interval) Union(b Interval) (Interval, error) {
+	if a.End < b.Start || b.End < a.Start {
 		return Interval{}, errors.New("No union")
 	}
 
-	return Interval{min(a.start, b.start), max(a.end, b.end)}, nil
+	return Interval{min(a.Start, b.Start), max(a.End, b.End)}, nil
 }
 
-func (a Interval) contains(b Interval) bool {
-	return a.start <= b.start && b.end <= a.end
+func (a Interval) Contains(b Interval) bool {
+	return a.Start <= b.Start && b.End <= a.End
 }
 
-func (a Interval) equals(b Interval) bool {
-	return a.start == b.start && a.end == b.end
+func (a Interval) Equals(b Interval) bool {
+	return a.Start == b.Start && a.End == b.End
 }
 
-func (a Interval) intersects(b Interval) bool {
-	return a.end >= b.start && b.end >= a.start
+func (a Interval) Intersects(b Interval) bool {
+	return a.End >= b.Start && b.End >= a.Start
 }
 
 func (a Interval) String() string {
-	return "[" + string(a.start) + "," + string(a.end) + "]"
+	return "[" + string(a.Start) + "," + string(a.End) + "]"
+}
+
+func (a Interval) Less(b Interval) bool {
+	return a.Start < b.Start
 }

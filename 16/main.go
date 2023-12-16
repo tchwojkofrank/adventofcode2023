ab255d3c741e215fac0dad85d0a8bb06f9c7c1b6
tchwojkofrank <tom@chwojkofrank.com>
2023-12-15 22:46:32 -0800

    Day 16 part 1

--- 16/main.go
+++ 16/main.go
@@ -37,8 +37,159 @@ func main() {
 	fmt.Printf("Running time: %v\n", end.Sub(start))
 }
 
+type Point struct {
+	x int
+	y int
+}
+
+type Direction struct {
+	dx int
+	dy int
+}
+
+type Vector struct {
+	p Point
+	d Direction
+}
+
+type Beam []Vector
+
+func mapInput(input string) (map[Point]rune, int, int) {
+	grid := make(map[Point]rune)
+	lines := strings.Split(input, "\n")
+	height := len(lines)
+	width := len(lines[0])
+	for y, line := range lines {
+		for x, c := range line {
+			grid[Point{x, y}] = c
+		}
+	}
+	return grid, width, height
+}
+
+func nextVector(v Vector, r rune) (Vector, *Vector) {
+	newVector := v
+	switch r {
+	case '|':
+		if v.d.dx == 0 {
+			newVector.p.x += newVector.d.dx
+			newVector.p.y += newVector.d.dy
+			return newVector, nil
+		} else {
+			teeVector := Vector{newVector.p, Direction{0, 1}}
+			newVector.d = Direction{0, -1}
+			newVector.p.x += newVector.d.dx
+			newVector.p.y += newVector.d.dy
+			return newVector, &teeVector
+		}
+	case '-':
+		if v.d.dy == 0 {
+			newVector.p.x += newVector.d.dx
+			newVector.p.y += newVector.d.dy
+			return newVector, nil
+		} else {
+			teeVector := Vector{newVector.p, Direction{1, 0}}
+			newVector.d = Direction{-1, 0}
+			newVector.p.x += newVector.d.dx
+			newVector.p.y += newVector.d.dy
+			return newVector, &teeVector
+		}
+	case '\\':
+		if v.d.dx == 0 {
+			if v.d.dy == 1 {
+				newVector.d = Direction{1, 0}
+			} else {
+				newVector.d = Direction{-1, 0}
+			}
+		} else {
+			if v.d.dx == 1 {
+				newVector.d = Direction{0, 1}
+			} else {
+				newVector.d = Direction{0, -1}
+			}
+		}
+		newVector.p.x += newVector.d.dx
+		newVector.p.y += newVector.d.dy
+		return newVector, nil
+	case '/':
+		if v.d.dx == 0 {
+			if v.d.dy == 1 {
+				newVector.d = Direction{-1, 0}
+			} else {
+				newVector.d = Direction{1, 0}
+			}
+		} else {
+			if v.d.dx == 1 {
+				newVector.d = Direction{0, -1}
+			} else {
+				newVector.d = Direction{0, 1}
+			}
+		}
+		newVector.p.x += newVector.d.dx
+		newVector.p.y += newVector.d.dy
+		return newVector, nil
+	default:
+		newVector.p.x += newVector.d.dx
+		newVector.p.y += newVector.d.dy
+		return newVector, nil
+	}
+}
+
+func traceBeams(grid map[Point]rune, vectorsToTrace []Vector, energizedPoints *map[Point]bool, width int, height int) {
+	vector := vectorsToTrace[0]
+	mappedVectors := make(map[Vector]struct{})
+	(*energizedPoints)[vector.p] = true
+	mappedVectors[vector] = struct{}{}
+	for len(vectorsToTrace) > 0 {
+		newVector, newTee := nextVector(vector, grid[vector.p])
+		if newTee != nil {
+			if _, ok := mappedVectors[*newTee]; !ok {
+				vectorsToTrace = append(vectorsToTrace, *newTee)
+			}
+		}
+		_, mapped := mappedVectors[newVector]
+		if newVector.p.x >= 0 && newVector.p.y >= 0 && newVector.p.x < width && newVector.p.y < height && !mapped {
+			(*energizedPoints)[newVector.p] = true
+			mappedVectors[newVector] = struct{}{}
+			vector = newVector
+		} else {
+			vectorsToTrace = vectorsToTrace[1:]
+			if len(vectorsToTrace) > 0 {
+				vector = vectorsToTrace[0]
+			}
+		}
+	}
+}
+
 func run(input string) string {
-	return ""
+	grid, width, height := mapInput(input)
+	newVector := Vector{Point{0, 0}, Direction{1, 0}}
+	energizedPoints := make(map[Point]bool)
+	vectorsToTrace := make([]Vector, 1)
+	vectorsToTrace[0] = newVector
+
+	traceBeams(grid, vectorsToTrace, &energizedPoints, width, height)
+	//count eneregized points
+	count := 0
+	for _, v := range energizedPoints {
+		if v {
+			count++
+		}
+	}
+	// for y := 0; y < height; y++ {
+	// 	for x := 0; x < width; x++ {
+	// 		if energizedPoints[Point{x, y}] {
+	// 			fmt.Printf("#")
+	// 		} else {
+	// 			fmt.Printf("%c", grid[Point{x, y}])
+	// 		}
+	// 	}
+	// 	fmt.Printf("\n")
+	// }
+
+	fmt.Printf("Energized points: %d\n", count)
+
+	return fmt.Sprintf("%d", count)
 }
 
 func run2(input string) string {
